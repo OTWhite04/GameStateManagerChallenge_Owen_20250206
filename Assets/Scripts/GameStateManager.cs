a7abc53471888e4f4a2d68ace454477abcbeb7cb
OTWhite04 <owen.t.white2005@gmail.com>
2025-02-11 13:48:05 -0400

    Fixed Pausing and Commented code

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameStateManager : MonoBehaviour
 {
+    //Reference for the game manager.
     public GameManager gameManager;
 
     public enum GameState
@@ -20,12 +21,14 @@ public class GameStateManager : MonoBehaviour
     //Store last value as a string.
     [SerializeField] private string lastStateDebug;
 
+    //Start method that calls te main menu at the start of the game.
     private void Start()
     {
         ChangeState(GameState.MainMenu_State);
         ChangeStateToMainMenu();
     }
     
+    //The change state method 
     public void ChangeState(GameState newState)
     {
         lastStateDebug = currentState.ToString();
@@ -37,46 +40,52 @@ public class GameStateManager : MonoBehaviour
         currentStateDebug = currentState.ToString();
     }
 
+    //Method that changes the game state back to the main menu.
     public void ChangeStateToMainMenu()
     {
         ChangeState(GameState.MainMenu_State);
         gameManager.uIManager.MainMenuUI();
     }
-    
+    //Pause method that stops time in game.
     public void Pause()
     {
         Time.timeScale = 0.0f;
     }
-
+    //Resume method that changes the timescale.
     public void Resume()
     {
         Time.timeScale = 1.0f;
     }
 
+    //The method for changing the state of the game to the gameplay.
     public void ChangeStateToGameplay()
     {
         ChangeState(GameState.Gameplay_State);
         gameManager.uIManager.GameplayUI();
     }
 
-
+    //Update method for changing game states.
     private void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.Escape) && currentState == GameState.Pause_State)
+        //If and else if statements for changing the states of the game.
+        if (Input.GetKeyDown(KeyCode.Escape) && currentState == GameState.Gameplay_State)
         {
             Debug.Log("Changed to Pause");
+            ChangeState(GameState.Pause_State);
             Pause();
+            
         }
 
        else if (Input.GetKeyDown(KeyCode.Escape) && currentState == GameState.Pause_State)
        {
             Debug.Log("Changed to Gameplay");
             Resume();
+            ChangeStateToGameplay();
        }
 
     }
 
+    //Method for handling all the state changes.
     private void HandleStateChange(GameState state)
     {
        switch(state)
@@ -100,6 +109,7 @@ public class GameStateManager : MonoBehaviour
        }
     }
 
+    //Method for quitting the application.
     public void Quit()
     {
         Application.Quit();
